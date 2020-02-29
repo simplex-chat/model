c48ee9cd4e66f3800cd606ad895996302f773149
Evgeny Poberezkin <e.poberezkin@me.com>
2020-02-29 15:18:52 +0000

    connection

--- src/Main.hs
+++ src/Main.hs
@@ -3,3 +3,29 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+data Client = Client
+  { name :: String 
+  }
+
+data Server = Server
+  { uri :: String -- create connection URI
+  }
+
+data ServerConnection = ServerConnection
+  { recipientId :: String
+  , senderId :: String 
+  }
+
+data ClientRecipientConnection = ClientRecipientConnection
+  { recipientURI :: String
+  }
+
+data ClientSenderConnection = ClientSenderConnection
+  { senderURI :: String
+  }
+
+data DuplexConnection = DuplexConnection
+  { receive :: ClientRecipientConnection
+  , send :: ClientSenderConnection
+  }
